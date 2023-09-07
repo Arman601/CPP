6dfa0694737cf4b2dd10fd4c6456262305d52ac8
Md Arman Hasan <123898576+Armgithu@users.noreply.github.com>
2023-09-07 21:52:13 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter your Age ";
+    cin>>age;
+    if(age>=18){
+    cout<<"you can vote";
+}
+    else{
+        cout<<"  you can't vote";
+    }
+    return 0;
+}
+    
+
