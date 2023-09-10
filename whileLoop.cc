ffd7d24a16113b4875b25e7ac99fd44a197064ce
Md Arman Hasan <123898576+Armgithu@users.noreply.github.com>
2023-09-10 14:28:52 +0530

    Add files via upload

---
+++ whileLoop.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     cout<<"counting 1 To 10: "<<endl;
+     cout<<"-----------------"<<endl;
+    int i = 1;
+    while( i <= 10){
+        cout<<i<<endl;
+        i++;
+    }
+   
+    return 0;
+}
