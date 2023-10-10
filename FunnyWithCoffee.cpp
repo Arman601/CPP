b91ee4a331d046775b5cc7ad43100c9b35a6c620
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2023-10-10 23:10:36 +0530

    Add files via upload

---
+++ FunnyWithCoffee.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string programmer = "programmer";
+    std::string beverage = "coffee";
+    
+    std::cout << "Why do " << programmer << "s prefer " << beverage << " in binary?" << std::endl;
+    std::cout << "Because they like it 0s and 1s!" << std::endl;
+    
+    return 0;
+}
