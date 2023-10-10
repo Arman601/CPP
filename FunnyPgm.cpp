2fe0d7ee361deea70149cd6e2f74db415e70b80e
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2023-10-10 23:08:45 +0530

    Add files via upload

---
+++ FunnyPgm.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main() {
+    std::cout << "Why don't scientists trust atoms?" << std::endl;
+    std::cout << "Because they make up everything!" << std::endl;
+
+    return 0;
+}
