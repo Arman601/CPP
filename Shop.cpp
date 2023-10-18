a40e5e88a5f5db48c6ed8912580e3183275b0cf9
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2023-10-18 23:49:46 +0530

    Add files via upload

---
+++ Shop.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class shop
+{
+    int itemId[100];
+    int itemprice[100];
+    int counter;
+
+public:
+    void initcounter(void) { counter = 0; }
+    void setprice(void);
+    void displayprice(void);
+};
+void shop ::setprice(void)
+{
+    cout << "Enter Id of your items:  ";
+    cin >> itemId[counter];
+
+    cout << "Enter price  of your items no  " << counter + 1 << endl;
+    cin >> itemprice[counter];
+    counter++;
+}
+void shop::displayprice(void)
+{
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "the price of items with Id " << itemId[i] << "  is " << itemprice[i] << endl;
+    }
+}
+int main()
+{
+    shop dukaan;
+    dukaan.initcounter();
+    dukaan.setprice();
+    dukaan.setprice();
+    dukaan.setprice();
+    dukaan.displayprice();
+    return 0;
+}
