a0005c634018b5e1b3af60a22c8655cfa7e644b1
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2023-11-05 19:06:19 +0530

    Add files via upload

---
+++ SwitchCase.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int day;
+    cout << "Enter the Number: ";
+    cin >> day;
+    switch (day)
+    {
+    case 1:
+        cout << "Sunday";
+        break;
+        switch (day)
+            ;
+    case 2:
+        cout << "Monday";
+        break;
+        switch (day)
+            ;
+    case 3:
+        cout << "Tuesday";
+        break;
+        switch (day)
+            ;
+    case 4:
+        cout << "Wednesday";
+        break;
+        switch (day)
+            ;
+    case 5:
+        cout << "Thursday";
+        break;
+        switch (day)
+            ;
+    case 6:
+        cout << "Friday";
+        break;
+        switch (day)
+            ;
+    case 7:
+        cout << "Saturday";
+        break;
+
+    default:
+        cout << "you Given wrong input ";
+
+        return 0;
+    }
+}
