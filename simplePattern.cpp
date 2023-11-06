8ebb56c43f3a533434a4a5dce0bc4c65c88dae0d
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2023-11-06 19:25:58 +0530

    Add files via upload

---
+++ simplePattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col;
+    cout<<"Enter the row";
+    cin>>row;
+    cout<<"Enter the column";
+    cin>>col;
+
+    for(int i = 1;i<=row;i++){
+    for(int j =1; j<=col;j++){
+
+    
+    
+    cout<<"*";
+    }
+    
+    cout<<endl;
+    }
+    return 0;
+
+}
